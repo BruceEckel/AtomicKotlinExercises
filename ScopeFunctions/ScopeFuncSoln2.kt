02ae575d4f3e874d6ffc17960db47e88297cc194
Bruce Eckel <BruceTEckel@gmail.com>
2020-09-02 22:54:00 -0600

    Successful auto-extraction

---
+++ ScopeFunctions/ScopeFuncSoln2.kt
@@ -0,0 +1,52 @@
+// ScopeFunctions/ScopeFuncSoln2.kt
+import classdelegation.SpaceShipControls
+import atomictest.*
+
+fun main() {
+  var velocity = 1
+  with(SpaceShipControls()) {
+    trace(forward(velocity))
+    trace(right(velocity))
+    trace(down(velocity))
+    this
+  }.let {
+    velocity++
+    trace(it.forward(velocity))
+    trace(it.right(velocity))
+    trace(it.down(velocity))
+    it
+  }.run {
+    velocity++
+    trace(forward(velocity))
+    trace(right(velocity))
+    trace(down(velocity))
+    this
+  }.apply {
+    velocity++
+    trace(forward(velocity))
+    trace(right(velocity))
+    trace(down(velocity))
+  }.also {
+    velocity++
+    trace(it.forward(velocity))
+    trace(it.right(velocity))
+    trace(it.down(velocity))
+  }
+  trace eq """
+    forward 1
+    right 1
+    down 1
+    forward 2
+    right 2
+    down 2
+    forward 3
+    right 3
+    down 3
+    forward 4
+    right 4
+    down 4
+    forward 5
+    right 5
+    down 5
+  """
+}
